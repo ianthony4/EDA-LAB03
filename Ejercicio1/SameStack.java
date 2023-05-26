718bd3625301de90936afde47a8a89d9fa9f02a5
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-26 08:14:44 -0500

    apilando las pilas con elementos INTEGER

--- Ejercicio1/SameStack.java
+++ Ejercicio1/SameStack.java
@@ -10,6 +10,21 @@ public class SameStack{
     }
     public static void apilar(Stack<Integer> ha, Stack<Integer> hb, Stack<Integer> hc){
         //Llenamos la pilas con el ejemplo propuesto en el ejercicio
+        //Para h1 (ha)
+        ha.push(1);
+        ha.push(1);
+        ha.push(1);
+        ha.push(2);
+        ha.push(3);
+        //Para h2 (hb)
+        hb.push(2);
+        hb.push(3);
+        hb.push(4);
+        //para h3 (hc)
+        hc.push(1);
+        hc.push(4);
+        hc.push(1);
+        hc.push(1);
         
     }
 
