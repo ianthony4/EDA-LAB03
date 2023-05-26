d3defe5cf657830fa16c77f04f49f071b3388ce1
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-26 08:08:16 -0500

    Creando las pilas restantes (h1,h2,h3)

--- Ejercicio1/SameStack.java
+++ Ejercicio1/SameStack.java
@@ -3,5 +3,8 @@ public class SameStack{
     public static void main(String[] args){
         //Creacion de las pilas
         Stack<Integer> h1 = new Stack<>();
+        Stack<Integer> h2 = new Stack<>();
+        Stack<Integer> h3 = new Stack<>();
     }
+
 }
