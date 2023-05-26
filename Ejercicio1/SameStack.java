71b326542c3ef6cad452a5c31701b0d26c4dc1c0
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-26 08:10:34 -0500

    Creando el metodo que apilas las pilas

--- Ejercicio1/SameStack.java
+++ Ejercicio1/SameStack.java
@@ -5,6 +5,12 @@ public class SameStack{
         Stack<Integer> h1 = new Stack<>();
         Stack<Integer> h2 = new Stack<>();
         Stack<Integer> h3 = new Stack<>();
+        //Llenar pilas (apilacion)
+        
+    }
+    public static void apilar(Stack<Integer> h1, Stack<Integer> h2, Stack<Integer> h3){
+        //Llenamos la pilas con el ejemplo propuesto en el ejercicio
+        
     }
 
 }
