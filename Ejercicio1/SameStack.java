ec4805a95a3ad55ba14a7e3b29c134f833773b92
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-02 08:14:38 -0500

    Creacion de los vectores con el ejemplo del ejercicio

--- Ejercicio1/SameStack.java
+++ Ejercicio1/SameStack.java
@@ -1,6 +1,9 @@
 
 public class SameStack{
     public static void main(String[] args){
+        int[] h1 = {1,1,1,2,3};
+        int[] h2 = {2,3,4};
+        int[] h3 = {1,4,1,1};
         
     }
     
