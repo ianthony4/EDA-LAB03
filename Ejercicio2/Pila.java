bd9fc8abdb0fc1f9cfb85c3aec1e80c816194de9
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-02 10:30:24 -0500

    Metodo push() para insertar un elemento y el siguiente Node será null, en caso haya más avanzamos y lo colocamos al final

--- Ejercicio2/Pila.java
+++ Ejercicio2/Pila.java
@@ -1,4 +1,4 @@
-public class Pila<T>{
+public class Pila<T> {
   private Node<T> top;
   private int count;
   
@@ -8,5 +8,18 @@ public class Pila<T>{
   }
   public boolean empty(){
     return this.count == 0;
+  }
+  public T push(T item){
+    if(this.empty())
+      this.top = new Node<T>(item);
+    else{
+      Node<T> temp = this.top;
+      while(temp.getNextNode() != null)
+        temp = temp.getNextNode();
+      temp.setNextNode(new Node<T>(item));
+      this.count++;
+    }
+    // Segun los estandares de java se retorna el elemento
+    return item;
   }
 }
