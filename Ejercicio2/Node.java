968e5abee013bfa89eade3a6f214bbf5d5351684
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-02 09:02:50 -0500

    Se agrego los constructores de Clase Node

--- Ejercicio2/Node.java
+++ Ejercicio2/Node.java
@@ -2,4 +2,12 @@ public class Node<T> {
   private T data;
   private Node<T> nextNode;
 
+  public Node(T data, Node<T> nextNode){
+    this.data = data;
+    this.nextNode = nextNode;
+  }
+  public Node(T data){
+    this(data, null);
+  }
+
 }
