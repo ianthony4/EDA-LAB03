caf35c058a1614abee9ea5cc19191e7adab73bf8
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-02 10:17:15 -0500

    Metodo empty() que devuelve true si hay 0 elementos, en caso contrario false

--- Ejercicio2/Pila.java
+++ Ejercicio2/Pila.java
@@ -1,9 +1,12 @@
-public class Pila<T>(){
+public class Pila<T>{
   private Node<T> top;
   private int count;
   
   public Pila(){
     this.top = null;
     this.count = 0;
+  }
+  public boolean empty(){
+    return this.count == 0;
   }
 }
