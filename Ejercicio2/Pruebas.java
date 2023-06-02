df36f23efcfa8036e6f456dae4719fd298975827
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-02 10:50:30 -0500

    declaraciones en Pruebas.java para probar

--- Ejercicio2/Pruebas.java
+++ Ejercicio2/Pruebas.java
@@ -1,5 +1,10 @@
 public class Pruebas{
   public static void main(String[] args){
+    Pila<String> libros = new Pila<String>();
+    if(libros.empty())
+      System.out.println("Esta vacío");
+    // ingresar elmentos
+    libros.push("Calculo I");
 
   }
 }
