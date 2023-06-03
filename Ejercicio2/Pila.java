05206d54052d2370d5faa2a13e16edbf115c96c6
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-03 11:55:51 -0500

    Metodo pop() con menos complejidad, atributo count eliminado, atributo Node root agregado

--- Ejercicio2/Pila.java
+++ Ejercicio2/Pila.java
@@ -1,32 +1,28 @@
 public class Pila<T> {
   private Node<T> top;
-  private int count;
+  private Node<T> root;
   
   public Pila(){
     this.top = null;
-    this.count = 0;
+    this.root = null;
   }
   public boolean empty(){
-    return this.count == 0;
+    return this.top == null;
   }
   public T peek(){
     return this.top.getData(); 
     
   }
   public T pop(){
-    Node<T> temp = this.top;
-    while(temp.getNextNode().getNextNode() != null)
-      temp = temp.getNextNode();
-    T data = temp.getNextNode().getData();
-    temp.setNextNode(null);
-    return data;
+    T temp = this.top.getData();
+    this.top = this.top.getNextNode();
+    return temp;
 
   }
   public T push(T item){
-    // Para evitar que la Pila sea más compleja,
+    // Para evitar que poner items sea complejo,
     // la estructura será la siguiente para nuevos items
     this.top = new Node<T>(item, this.top);
-    this.count++;
     // Segun los estandares de java se retorna el elemento
     return item;
   }
@@ -34,7 +30,7 @@ public class Pila<T> {
   public String toString(){
     String res = "{ ";
     for(Node<T> temp = this.top; temp != null; temp = temp.getNextNode())
-      res += temp.toString() + ", ";
+      res += ", " + temp.toString();
     return res + " }";
   }
 }
