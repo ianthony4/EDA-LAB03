a0db97b7b76ef28b6d316026c9a9e2ebfbc009a6
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-04 09:27:14 -0500

    Terminando la funcion, se agregaron comentarios para mas detalles

--- Ejercicio1/SameStack.java
+++ Ejercicio1/SameStack.java
@@ -14,7 +14,15 @@ public class SameStack{
     }
     //Altura incrementales, por nivel de un arreglo dado
     public static int[] sumaxNivel(int[] h){
-    
+        int[] sumIncremental = new int[h.length];
+        int laSuma = 0;
+        //recorremos todo el arreglo recolectando y acumulando la suma
+        for(int i = 0;i<h.length;i++){
+            laSuma += h[i];
+            sumIncremental[i] = laSuma;
+        }
+        //retornamos el arreglo de sumas acumuladas
+        return sumIncremental;
     }
 
 
