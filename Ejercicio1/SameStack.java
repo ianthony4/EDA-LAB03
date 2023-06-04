5c06bd1cabf23836edf075da29dbdf8aae80a15c
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-04 09:23:02 -0500

    Dando formato al codigo y colocando comentarios

--- Ejercicio1/SameStack.java
+++ Ejercicio1/SameStack.java
@@ -1,17 +1,16 @@
 
 public class SameStack{
-        public static void main(String[] args){
-            int[] h1 = {1,1,1,2,3};
-            int[] h2 = {2,3,4};
-            int[] h3 = {1,4,1,1};
-            //Corregir arroja 6
-            System.out.println(equalStack(h1, h2, h3));
+    public static void main(String[] args){
+        int[] h1 = {1,1,1,2,3};
+        int[] h2 = {2,3,4};
+        int[] h3 = {1,4,1,1};
+        //Corregir arroja 6
+        System.out.println(equalStack(h1, h2, h3));
+    }
 
-            
-        }
-        //Funcion que calculara que las alturas de los vectores sean igualews
-        public static int equalStack(int[] ha, int[] hb, int[] hc){
-            int rpta = 0;
+    //Funcion que calculara que las alturas de los vectores sean igualews
+    public static int equalStack(int[] ha, int[] hb, int[] hc){
+        int rpta = 0;
         for (int i = 0; i < ha.length; i++) {
             for (int j = 0; j < hb.length; j++) {
                 for (int k = 0; k < hc.length; k++) {
@@ -22,27 +21,25 @@ public class SameStack{
             }
         }
         return rpta;
-        }
-        //Funcion que deveulve la suma sin un elemento del vector
-        public static int sumaSinElemento(int[] hx, int idx) {
-            int[] arrAux = new int[hx.length - 1];
-            for (int i = 0, j = 0; i < hx.length; i++) {
-                if (idx != i) {
-                    arrAux[j] = hx[i];
-                    j++;
-                }
+    }
+    //Funcion que deveulve la suma sin un elemento del vector
+    public static int sumaSinElemento(int[] hx, int idx) {
+        int[] arrAux = new int[hx.length - 1];
+        for (int i = 0, j = 0; i < hx.length; i++) {
+            if (idx != i) {
+                arrAux[j] = hx[i];
+                j++;
             }
-            int sumaTotal = sumatorio(arrAux);
-            return sumaTotal;
         }
-
-        public static int sumatorio(int[] hx) {
-            int suma = 0;
-            for (int i = 0; i < hx.length; i++) {
-                suma += hx[i];
-            }
-            return suma;
+        int sumaTotal = sumatorio(arrAux);
+        return sumaTotal;
+    }
+    //devuelve la sumatoria TOTAL de un arreglo
+    public static int sumatorio(int[] hx) {
+        int suma = 0;
+        for (int i = 0; i < hx.length; i++) {                
+            suma += hx[i];
         }
-    
-
+        return suma;
+    }    
 }
