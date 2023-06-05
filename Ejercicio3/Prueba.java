61d7d33d5591775e22efe05e9084c9be769400d0
kllacma@unsa.edu.pe <kllacma@unsa.edu.pe>
2023-06-04 21:35:30 -0500

    revision

--- Ejercicio3/Prueba.java
+++ Ejercicio3/Prueba.java
@@ -3,21 +3,21 @@ package Ejercicio3;
 public class Prueba {
    public static void main(String[] args) {
      Cola<Integer> queue = new Cola<>();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
 
-System.out.println(queue.peek()); // Output: 1
+    System.out.println(queue.peek()); 
 
-int item = queue.dequeue();
-System.out.println(item); // Output: 1
+    int item = queue.dequeue();
+    System.out.println(item); 
 
-System.out.println(queue.isEmpty()); // Output: false
+    System.out.println(queue.isEmpty()); 
 
-queue.dequeue();
-queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
 
-System.out.println(queue.isEmpty());
+    System.out.println(queue.isEmpty());
    } 
    
 }
